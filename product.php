2f926f07bfa760afbfcdae64f0fa3805478817de
Grenbui <91963277+Grenbui@users.noreply.github.com>
2023-04-28 14:19:21 +0700

    Update product.php

--- product.php
+++ product.php
@@ -21,136 +21,8 @@
     </head>
     
     <body>
-        <div id="header">
-            <div class="header-top container-fluid px-0">
-                <!-- <div class="row"> -->
-                    <div class="header-top-navbar">
-                        <div class="header-top-navbar-img">
-                            <img src="./Image/logo.webp" alt="Logo" class="small-image">
-                        </div>
-                        <div class="header-top-navbar-ul">
-                            <ul class="header-top-navbar-right-list">
-                                <li class="header-top-navbar-item" id="search-modal">
-                                    <div class="input-group">
-                                        <input type="text" value="" placeholder="Tìm kiếm" id="search-input-form" class="input-group-field">
-                                        <!-- <input type="hidden" name="contact[tags]" value="newsletter"> -->
-                                        <span class="input-group-btn">
-                                            <button class="btn-more-white" type="submit">
-                                                <i class="fa-solid fa-magnifying-glass"></i>
-                                            </button>
-                                        </span>
-                                    </div>
-                                </li>
-                                <li onclick="openCloseSearch()" class="header-top-navbar-item">
-                                    <a href="#" class="header-top-navbar-link  hover-underline-text" id="search-btn">Tìm kiếm
-                                        <i class="fa-solid fa-magnifying-glass"></i>
-                                    </a>
-                                    <!-- <div id="search-block">
-                                        <form>
-                                          <input type="text" placeholder="Nhập từ khóa tìm kiếm">
-                                          <button type="submit">Tìm kiếm</button>
-                                        </form>
-                                    </div> -->
-                                    
-                                </li>
-                                <li class="header-top-navbar-item">
-                                    <a href="" class="header-top-navbar-link hover-underline-text">Liên hệ</a>
-                                </li>
-                                <li class="header-top-navbar-item">
-                                    <a href="" class="header-top-navbar-link">
-                                        <i class="fa-solid fa-user"></i>
-                                    </a>
-                                </li>
-                                <li class="header-top-navbar-item">
-                                    <a href="" class="header-top-navbar-link">
-                                        <i class="fa-solid fa-cart-shopping"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                       
-                    </div>
-                    
-                <!-- </div> -->
-            
-            </div>
-            <div class="header-under-navbar">
-                <ul class="header-under-navbar-list">
-                    <li class="header-under-navbar-item">
-                        <a href="" class="header-under-navbar-link hover-underline-text">Trang chủ</a>
-                    </li>
-                    <li class="header-under-navbar-item">
-                        <a href="#" class="header-under-navbar-link hover-underline-text">Sản phẩm nam (Men) 
-                            <i class="fa-solid fa-angle-down"></i>
-                        </a>
-                        <ul class="subnav-dropdown-menu">
-                            <li class="subnav-item">
-                                <a href="#" class="subnav-item-link">Footwears (Giày/ dép)
-                                    <i class="fa-solid fa-chevron-right"></i>
-                                </a>
-                                <ul class="sub-subnav-dropdown-menu">
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Sparkling slippers (Giày thời trang)</a>
-                                    </li>
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Dress shoes (Giày tây công sở)</a>
-                                    </li>
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Comfort sneakers (Giày thể thao)</a>
-                                    </li>
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Mocassins/ Drivers (Giày lười)</a>
-                                    </li>
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Backless (Dép)</a>
-                                    </li>
-                                    <li class="subnav-item">
-                                        <a href="" class="subnav-item-link">Handmade leather soles (Giày đế da may tay)</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Belts (Thắt lưng)</a>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Bags/Clutches/Backpack (Túi/ Ba lô)</a>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Small leather stuff (Ví/ Phụ kiện)</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="header-under-navbar-item">
-                        <a href="" class="header-under-navbar-link hover-underline-text">Sản phẩm nữ (Women)
-                            <i class="fa-solid fa-angle-down"></i>
-                        </a>
-                        <ul class="subnav-dropdown-menu">
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Footwears (Giày/ dép)</a>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Belts (Thắt lưng)</a>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Bags/Clutches/Backpack (Túi/ Ba lô)</a>
-                            </li>
-                            <li class="subnav-item">
-                                <a href="" class="subnav-item-link">Small leather stuff (Ví/ Phụ kiện)</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="header-under-navbar-item">
-                        <a href="" class="header-under-navbar-link hover-underline-text">Sản phẩm khuyến mãi (Sale off items)</a>
-                    </li>
-                    <li class="header-under-navbar-item">
-                        <a href="" class="header-under-navbar-link hover-underline-text">FAQS (Hỏi đáp)</a>
-                    </li>
-                    <li class="header-under-navbar-item">
-                        <a href="" class="header-under-navbar-link hover-underline-text">Giới thiệu</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <?php include 'header.php'; ?>
+
         <section id="breadcrump-wrapper">
             <div class="breadcrumb-overlay"></div>
             <div class="breadcrumb-content text-center">
@@ -414,6 +286,10 @@
                 </div>
             </div>
         </div>
+
+        <?php 
+            include 'footer.php'; 
+        ?>
     </body>
 
     <script src="./Javascript/collections.js"></script>
