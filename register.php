d6aec989339407e15ce4af780d83fc744bf40411
Grenbui <91963277+Grenbui@users.noreply.github.com>
2023-05-03 17:44:26 +0700

    update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" type="image/png" href="./Image/favicon.webp">
     
     <link rel="stylesheet" href="./CSS/account.css">
-    <link rel="stylesheet" href="./Framework/bootstrap/bootstrap-4.0.0-dist/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="./Framework/bootstrap/css.css">
     <link rel="stylesheet" href="./Framework/bootstrap/css.css">
     <link rel="stylesheet" href="./Framework/bootstrap/js.js">
     <link rel="stylesheet" href="./Font/fontawesome-free-6.2.0-web/css/all.css">
@@ -16,6 +16,34 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
+    
+    <?php
+        include './ConnectDatabase/connectDatabase.php';
+        try{
+            if(isset($userName) && isset($email) && isset($password) && isset($firstName) && isset($lastName)){
+                $userName = $_POST['userName'];
+                $email = $_POST['email'];
+                $phoneNum = $_POST['phoneNum'];
+                $password = $_POST['password'];
+            
+                $firstName = $_POST['firstName'];
+                $lastName = $_POST['lastName'];
+                $name = $firstName . $lastName;
+            }
+            
+            $stmt = $conn->prepare("INSERT INTO CUSTOMER VALUES ('c90',:name,:email,:userName,:password,:phoneNum,'','','','',0)");
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':phoneNum', $phoneNum);
+            $stmt->bindParam(':password', $password);
+            $stmt->bindParam(':userName', $userName);
+    
+            $stmt->execute();
+            $conn = null;
+        } catch (PDOException $e) {
+            echo "Connection failed" . $e->getMessage();
+        }
+    ?> 
     
     <section id="breadcrump-wrapper">
         <div class="breadcrumb-overlay"></div>
@@ -28,34 +56,41 @@
         <div class="main-container">
             <section class="page-wrapper">
                 <div class="register-container">
-                    <form action="" class="register-customer">
+                    <form action="" class="register-customer" method="post">
                         <h2 class="text-center customer-header">Tạo tài khoản</h2>
                         <div class="form-group register-name">
                             <div class="sur-Name">
                                 <label for="SurName" class="form-label">Họ</label>
-                                <input type="text" class="form-control" placeholder="Nguyễn" required>
+                                <input type="text" name="surName" class="form-control" placeholder="Nguyễn" required>
                             </div>
                             
                             <div class="first-Name">
                                 <label for="firstName" class="form-label">Tên</label>
-                                <input type="text" class="form-control" placeholder="A" required>
+                                <input type="text" name="firstName" class="form-control" placeholder="A" required>
                             </div>
-                            
+
                         </div>
                         <div class="form-group">
-                            <label for="email" class="form-label">Địa chỉ Email</label>
-                            <input type="email" id="email" class="form-control" placeholder="abc@email.com" required>
+                            <label for="text" class="form-label">Tên tài khoản</label>
+                            <input type="text" id="phone" name="userName" class="form-control" placeholder="" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="tel" class="form-label">Số điện thoại</label>
+                            <input type="tel" id="phone" name="phoneNum" class="form-control" placeholder="0912345678" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" id="email" name="email" class="form-control" placeholder="abc@email.com" required>
                         </div>
                         <div class="form-group">
                             <label for="" class="form-label">Mật Khẩu</label>
-                            <input type="password" id="password" class="form-control" required>
+                            <input type="password" id="password" name="password" class="form-control" required>
                         </div>
                         <!-- <div class="optional">
                             <p><a href="" class="customer_register_link">Đăng kí</a></p>
                             <p><a href="" class="customer_forget_password_link">Quên mật khẩu?</a></p>
                         </div> -->
-                        <input class = "btn btn-dark w-100" type="submit" value="Đăng kí">
-                        
+                        <input class = "btn btn-dark w-100 submit__Btn" type="submit" value="Đăng kí">
                     </form>
                 </div>
             </section>
